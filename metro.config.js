eabd61f662c8b0bbeee57766a0693e07fd250aa6
roukayachelly <roukayachelly7@gmail.com>
2025-05-13 06:03:50 +0100

    update

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
